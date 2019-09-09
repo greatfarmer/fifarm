a71f0240c57c4e151215571538788f132efa97c5
Seonghun Kang <lutis107@gmail.com>
2019-09-10 00:01:14 +0900

    Remove '/map'

--- src/main/java/com/fifarm/spider/controller/HomeController.java
+++ src/main/java/com/fifarm/spider/controller/HomeController.java
@@ -15,11 +15,8 @@ import com.fifarm.spider.service.HomeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import java.util.Map;
 
 @Controller
 public class HomeController {
@@ -41,13 +38,6 @@ public class HomeController {
 
     @Autowired
     ClubDbHandler clubDbHandler;
-
-    @GetMapping("/map")
-    public String map(Model model) throws Exception {
-        Map<String, Object> map = homeService.jsonToMap();
-        model.addAttribute("map", map);
-        return "map";
-    }
 
     @RequestMapping("/player")
     public String name(@RequestParam("name") String name, Model model) throws Exception {
