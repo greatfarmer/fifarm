f23a4272f2710a20bb61988e2b9c001ee0696576
Seonghun Kang <lutis107@gmail.com>
2020-01-05 14:20:43 +0900

    Organize import

--- src/main/java/com/fifarm/spider/controller/JsonController.java
+++ src/main/java/com/fifarm/spider/controller/JsonController.java
@@ -7,7 +7,11 @@ import com.fifarm.spider.service.MongoService;
 import com.mongodb.DBCursor;
 import com.mongodb.DBObject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
