c82cb8f745914ab62f08bc643073eb59c41999b7
Seonghun Kang <lutis107@gmail.com>
2020-03-13 11:47:57 +0900

    Add ControllerTests

---
+++ src/test/java/net/fifarm/spider/controller/PathControllerTest.java
@@ -0,0 +1,37 @@
+package net.fifarm.spider.controller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class PathControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    public void searchByName() throws Exception {
+        mockMvc.perform(get("/search/son"))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void linkById() throws Exception {
+        mockMvc.perform(get("/player/251858344"))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+}
